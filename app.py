01f821010841a35cccdb6a18069e116a67de31f2
Douglas Bender <benderd@flip1.engr.oregonstate.edu>
2022-07-26 13:38:57 -0700

    Through Step 6: added route to display bsg-people

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
 import database.db_connector as db
 
@@ -9,39 +9,21 @@ db_connection = db.connect_to_database()
 
 # Routes 
 
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
-
 @app.route('/')
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
+
+@app.route('/bsg-people')
+def bsg_people():
+
+    # Write the query and save it to a variable
+    query = "SELECT * FROM bsg_people;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("bsg.j2", bsg_people=results)
 
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8027))
+    port = int(os.environ.get('PORT', 8028))
     app.run(port=port, debug=True) 
